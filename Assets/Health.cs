f8b093b6852d56a106fa131b990b6c3e9ce53df0
onixus74 <marwen.ounis@esprit.tn>
2014-12-16 21:12:21 +0100

    First & Last Push

---
+++ Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class Health : MonoBehaviour {
+    public Scrollbar healthBar;
+    public float health = 100;
+    public void TakeDamage(float value)
+    {
+        health -= value;
+        healthBar.size = health / 100f;
+    }
+
+
+    void Update()
+    {
+        if ((Advertisement.isReady()))
+        {   
+            if(health<=0)
+            Advertisement.Show();
+            if(Advertisement.isShowing)
+            health = 100;
+            Debug.Log("Showing ads");
+        }
+    }
+
+}
